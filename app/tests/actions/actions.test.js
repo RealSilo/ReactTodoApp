db54e53fa64494e92d97a570884becb5ceae3c5a
RealSilo <szilard.magyar@gmail.com>
2016-08-04 23:27:25 +0200

    adding authReducer

--- app/tests/actions/actions.test.js
+++ app/tests/actions/actions.test.js
@@ -86,6 +86,26 @@ describe('Actions', () => {
     expect(res).toEqual(action);
   });
 
+  it('should generate login action', () => {
+    const action = {
+      type: 'LOGIN',
+      uid: '1234abc'
+    };
+    const res = actions.login(action.uid);
+
+    expect(res).toEqual(action);
+  });
+
+  it('should generate logout action', () => {
+    const action = {
+      type: 'LOGOUT'
+    };
+    const res = actions.logout();
+
+    expect(res).toEqual(action);
+  });
+
+
   describe('Tests with firebase todos', () => {
     var testTodoRef;
 
--- app/tests/components/todo_app.test.js
+++ app/tests/components/todo_app.test.js
@@ -10,19 +10,19 @@ import {TodoApp} from 'TodoApp';
 import TodoList from 'TodoList';
 
 describe('TodoApp', () => {
-  it('should exist', () =>{
+  it('should exist', () => {
     expect(TodoApp).toExist();
   });
 
-  it('should render todo list', () => {
+  it('should render TodoList', () => {
     var store = configureStore.configure();
     var provider = TestUtils.renderIntoDocument(
       <Provider store={store}>
         <TodoApp/>
       </Provider>
-      );
+    );
 
-    var todoApp = TestUtils.scryRenderedComponentsWithType(provider, TodoApp)[0];
+    var todoApp = TestUtils.scryRenderedComponentsWithType(provider, TodoApp)[0]
     var todoList = TestUtils.scryRenderedComponentsWithType(todoApp, TodoList);
 
     expect(todoList.length).toEqual(1);
